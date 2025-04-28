6454d44b2fcd24633e4d1048f588520c7abf2f5d
sandeephere08 <sandeepsharma13082003@gmail.com>
2025-04-28 22:45:00 +0530

    fix: update import statements in mention.py and shayari.py

---
+++ modules/shayari.py
@@ -0,0 +1,154 @@
+from ZEFMUSIC import app
+import asyncio
+import random
+from pyrogram import Client, filters
+from pyrogram.enums import ChatType, ChatMemberStatus
+from pyrogram.errors import UserNotParticipant
+
+spam_chats = []
+
+EMOJI = [
+    "🦋🦋🦋🦋🦋",
+    "🧚🌸🧋🍬🫖",
+    "🥀🌷🌹🌺💐",
+    "🌸🌿💮🌱🌵",
+    "❤️💚💙💜🖤",
+    "💓💕💞💗💖",
+    "🌸💐🌺🌹🦋",
+    "🍔🦪🍛🍲🥗",
+    "🍎🍓🍒🍑🌶️",
+    "🧋🥤🧋🥛🍷",
+    "🍬🍭🧁🎂🍡",
+    "🍨🧉🍺☕🍻",
+    "🥪🥧🍦🍥🍚",
+    "🫖☕🍹🍷🥛",
+    "☕🧃🍩🍦🍙",
+    "🍁🌾💮🍂🌿",
+    "🌨️🌥️⛈️🌩️🌧️",
+    "🌷🏵️🌸🌺💐",
+    "💮🌼🌻🍀🍁",
+    "🧟🦸🦹🧙👸",
+    "🧅🍠🥕🌽🥦",
+    "🐷🐹🐭🐨🐻‍❄️",
+    "🦋🐇🐀🐈🐈‍⬛",
+    "🌼🌳🌲🌴🌵",
+    "🥩🍋🍐🍈🍇",
+    "🍴🍽️🔪🍶🥃",
+    "🕌🏰🏩⛩️🏩",
+    "🎉🎊🎈🎂🎀",
+    "🪴🌵🌴🌳🌲",
+    "🎄🎋🎍🎑🎎",
+    "🦅🦜🕊️🦤🦢",
+    "🦤🦩🦚🦃🦆",
+    "🐬🦭🦈🐋🐳",
+    "🐔🐟🐠🐡🦐",
+    "🦩🦀🦑🐙🦪",
+    "🐦🦂🕷️🕸️🐚",
+    "🥪🍰🥧🍨🍨",
+    "🥬🍉🧁🧇",
+]
+
+SHAYRI = [
+    "🌺**बहुत अच्छा लगता है तुझे सताना और फिर प्यार से तुझे मनाना।**🌺 \n\n**🥀Bahut aacha lagta hai tujhe satana Aur fir pyar se tujhe manana.🥀**",
+    "🌺**मेरी जिंदगी मेरी जान हो तुम मेरे सुकून का दुसरा नाम हो तुम।**🌺 \n\n**🥀Meri zindagi Meri jaan ho tum Mere sukoon ka Dusra naam ho tum.🥀**",
+    "🌺**तुम मेरी वो खुशी हो जिसके बिना, मेरी सारी खुशी अधूरी लगती है।**🌺 \n\n**🥀Tum Meri Wo Khushi Ho Jiske Bina, Meri Saari Khushi Adhuri Lagti Ha.🥀**",
+    "🌺**काश वो दिन जल्दी आए,जब तू मेरे साथ सात फेरो में बन्ध जाए।**🌺 \n\n**🥀Kash woh din jldi aaye Jb tu mere sath 7 feron me bndh jaye.🥀**",
+    "🌺**अपना हाथ मेरे दिल पर रख दो और अपना दिल मेरे नाम कर दो।**🌺 \n\n**🥀apna hath mere dil pr rakh do aur apna dil mere naam kar do.🥀**",
+    "🌺**महादेव ना कोई गाड़ी ना कोई बंगला चाहिए सलामत रहे मेरा प्यार बस यही दुआ चाहिए।**🌺 \n\n**🥀Mahadev na koi gadi na koi bangla chahiye salamat rhe mera pyar bas yahi dua chahiye.🥀**",
+    "🌺**फिक्र तो होगी ना तुम्हारी इकलौती मोहब्बत हो तुम मेरी।**🌺 \n\n**🥀Fikr to hogi na tumhari ikloti mohabbat ho tum meri.🥀**",
+    "🌺**सुनो जानू आप सिर्फ किचन संभाल लेना आप को संभालने के लिए मैं हूं ना।**🌺 \n\n**🥀suno jaanu aap sirf kitchen sambhal lena ap ko sambhlne ke liye me hun naa.🥀**",
+    "🌺**सौ बात की एक बात मुझे चाहिए बस तेरा साथ।**🌺 \n\n**🥀So bat ki ek bat mujhe chahiye bas tera sath.🥀**",
+    "🌺**बहुत मुश्किलों से पाया हैं तुम्हें, अब खोना नहीं चाहते,कि तुम्हारे थे तुम्हारे हैं अब किसी और के होना नहीं चाहते।**🌺 \n\n**🥀Bahut muskilon se paya hai tumhe Ab khona ni chahte ki tumhare they tumhare hai ab kisi or k hona nhi chahte.🥀**",
+]
+
+@app.on_message(filters.command(["shayari"], prefixes=["/", "@", "#"]))
+async def mentionall(client, message):
+    chat_id = message.chat.id
+    if message.chat.type == ChatType.PRIVATE:
+        return await message.reply("๏ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘs.")
+
+    is_admin = False
+    try:
+        participant = await client.get_chat_member(chat_id, message.from_user.id)
+    except UserNotParticipant:
+        is_admin = False
+    else:
+        if participant.status in (
+            ChatMemberStatus.ADMINISTRATOR,
+            ChatMemberStatus.OWNER
+        ):
+            is_admin = True
+    if not is_admin:
+        return await message.reply("๏ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀᴅᴍɪɴ ʙᴀʙʏ, ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴛᴀɢ ᴍᴇᴍʙᴇʀs.")
+
+    if message.reply_to_message and message.text:
+        return await message.reply("/shayari ᴛʏᴘᴇ ʟɪᴋᴇ ᴛʜɪs / ʀᴇᴘʟʏ ᴀɴʏ ᴍᴇssᴀɢᴇ ɴᴇxᴛ ᴛɪᴍᴇ")
+    elif message.text:
+        mode = "text_on_cmd"
+        msg = message.text
+    elif message.reply_to_message:
+        mode = "text_on_reply"
+        msg = message.reply_to_message
+        if not msg:
+            return await message.reply("/shayari ᴛʏᴘᴇ ʟɪᴋᴇ ᴛʜɪs / ʀᴇᴘʟʏ ᴀɴʏ ᴍᴇssᴀɢᴇ ɴᴇxᴛ ᴛɪᴍᴇ")
+    else:
+        return await message.reply("/shayari ᴛʏᴘᴇ ʟɪᴋᴇ ᴛʜɪs / ʀᴇᴘʟʏ ᴀɴʏ ᴍᴇssᴀɢᴇ ɴᴇxᴛ ᴛɪᴍᴇ")
+
+    if chat_id in spam_chats:
+        return await message.reply("๏ ᴘʟᴇᴀsᴇ ᴀᴛ ғɪʀsᴛ sᴛᴏᴘ ʀᴜɴɴɪɴɢ ᴘʀᴏᴄᴇss...")
+
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+
+    try:
+        async for usr in client.get_chat_members(chat_id):
+            if not chat_id in spam_chats:
+                break
+            if usr.user.is_bot:
+                continue
+            usrnum += 1
+            usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}) "
+
+            if usrnum == 1:
+                if mode == "text_on_cmd":
+                    txt = f"{usrtxt} {random.choice(SHAYRI)}"
+                    await client.send_message(chat_id, txt)
+                elif mode == "text_on_reply":
+                    await msg.reply(f"[{random.choice(EMOJI)}](tg://user?id={usr.user.id})")
+                await asyncio.sleep(4)
+                usrnum = 0
+                usrtxt = ""
+    except Exception as e:
+        print(f"Error in shayari: {e}")
+    finally:
+        try:
+            spam_chats.remove(chat_id)
+        except:
+            pass
+
+@app.on_message(filters.command(["shstop", "shayarioff"]))
+async def cancel_spam(client, message):
+    if not message.chat.id in spam_chats:
+        return await message.reply("๏ ᴄᴜʀʀᴇɴᴛʟʏ ɪ'ᴍ ɴᴏᴛ ᴛᴀɢɢɪɴɢ ʙᴀʙʏ.")
+
+    is_admin = False
+    try:
+        participant = await client.get_chat_member(message.chat.id, message.from_user.id)
+    except UserNotParticipant:
+        is_admin = False
+    else:
+        if participant.status in (
+            ChatMemberStatus.ADMINISTRATOR,
+            ChatMemberStatus.OWNER
+        ):
+            is_admin = True
+
+    if not is_admin:
+        return await message.reply("๏ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀᴅᴍɪɴ ʙᴀʙʏ, ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴛᴀɢ ᴍᴇᴍʙᴇʀs.")
+
+    try:
+        spam_chats.remove(message.chat.id)
+    except:
+        pass
+    return await message.reply("๏ ᴘʀᴏᴄᴇss sᴛᴏᴘᴘᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ๏") 
